0e84157b2ef12b74950892e7b2788c53c14c6218
mustapha <mustaphastar067@gmail.com>
2025-01-15 16:34:43 +0100

    adding: styling log in page

--- public/sign-up.php
+++ public/sign-up.php
@@ -53,12 +53,6 @@
                       <label class="form-label">Password</label>
                       <input type="password" class="form-control">
                     </div>
-                    <div class="form-check form-check-info text-start ps-0">
-                      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
-                      <label class="form-check-label" for="flexCheckDefault">
-                        I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms and Conditions</a>
-                      </label>
-                    </div>
                     <div class="text-center">
                       <button type="button" class="btn btn-lg bg-gradient-dark btn-lg w-100 mt-4 mb-0">Sign Up</button>
                     </div>
